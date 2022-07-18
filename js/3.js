ded25430f781272399e26835ece4704e87980a73
samantha-teixeira <samerika69@gmail.com>
2022-07-18 15:33:20 +0200

    m

---
+++ js/3.js
@@ -0,0 +1,4 @@
+var largeur = prompt('Quelle est la largeur du rectangle?');
+var longueur = prompt('Quelle est la longueur du rectangle?');
+var aire = largeur * longueur;
+alert('L\'aire du rectangle est '+ aire );
